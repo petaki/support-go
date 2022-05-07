4c3ebe48b327cb7a81101999d12c6f9161976089
Peter Takacs <peter.takacs@koodilab.com>
2022-05-07 18:40:27 +0200

    CS fix

--- forms/regexp.go
+++ forms/regexp.go
@@ -3,7 +3,7 @@ package forms
 import "regexp"
 
 // UsernameRegexp regexp.
-var UsernameRegexp = regexp.MustCompile("^[a-zA-Z0-9\\.\\-_]+$")
+var UsernameRegexp = regexp.MustCompile(`^[a-zA-Z0-9\\.\\-_]+$`)
 
 // EmailRegexp regexp.
 var EmailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
